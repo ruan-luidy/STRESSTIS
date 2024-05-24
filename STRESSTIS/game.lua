871850804ee48e8701c314784f3447574595f623
ruan-luidy <ruan.liidy23@gmail.com>
2024-05-24 14:15:50 -0300

    Logica para o sharder + Aumento de velocidade
    Adicionei a logica do shader no .placePiece e aumentei a velocidade a cada mil pontos...
    preciso arrumar o sistema de pontuação desse lixo

--- STRESSTIS/game.lua
+++ STRESSTIS/game.lua
@@ -9,11 +9,13 @@ local moveTimer = 0
 local moveInterval = 0.1
 local gameOver = false
 local score = 0
+local blurShader = require("STRESSTIS.EFFECTS.blurShader")
 
 -- Esta função carrega o jogo
 function game.load()
     grid.initialize(20, 10, 30) -- Inicializa a grade
     pieces.initialize()         -- Inicializa as peças
+    pieces.loadSprites()        -- Carrega os sprites
     game.spawnNewPiece()        -- Gera uma nova peça
 end
 
@@ -25,6 +27,18 @@ function game.draw()
     end
     ui.draw(score, gameOver)                          -- Desenha a interface do usuário
 end                                                   
+
+-- Reseta todas as variáveis para recomeçar o jogo
+function game.restart()
+    dropTimer = 0
+    dropInterval = 0.5
+    moveTimer = 0
+    moveInterval = 0.1
+    gameOver = false
+    score = 0
+    grid.initialize(20, 10, 30)
+    game.spawnNewPiece()
+end
 
 -- Esta função atualiza o estado do jogo
 function game.update(dt)
@@ -57,7 +71,7 @@ end
 -- Esta função é chamada quando uma tecla é pressionada
 function game.keypressed(key)
     if key == "tab" then
-        love.event.quit("restart") -- Se a tecla Tab é pressionada, reinicia o jogo
+        game.restart() -- Se a tecla Tab é pressionada, reinicia o jogo
         return
     end
     if gameOver then
@@ -103,13 +117,29 @@ end
 
 -- Esta função coloca uma peça na grade
 function game.placePiece()
+    -- Aplica o sharder de desfoque
+    love.graphics.setShader(blurShader)
+
+    -- Colcoa a peça na grade
     grid.placePiece(currentPiece, currentX, currentY)
+
+    -- Desenha a grade e as peças
+    grid.draw()
+    pieces.draw(currentPiece, currentX, currentY)
+
+    -- Desativa o shader
+    love.graphics.setShader()
 end
 
 -- Esta função limpa as linhas completas da grade e atualiza a pontuação
 function game.clearLines()
     local linesCleared = grid.clearLines()
     score = score + 100 * linesCleared
+
+    -- Aumenta a velocidade a cada mil pontos
+    if score % 1000 == 0 then
+        dropInterval = dropInterval * 0.9 -- Reduz o intervlo de queda em 10%
+    end
 end
 
 -- Esta função gera uma nova peça e a coloca na posição inicial
